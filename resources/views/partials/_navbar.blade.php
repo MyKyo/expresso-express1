8b7f98679b7ef31a13e8fa20450f2687650bb2e7
Nama Anda <emailanda@contoh.com>
2025-09-17 16:37:51 +0700

    doks

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -23,6 +23,9 @@
                     <li class="nav-item">
                         <a class="nav-link text-center py-2" href="#blog">Blog</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-center py-2" href="#tim">Tim</a>
+                    </li>
                 </ul>
             </div>
         </div>
